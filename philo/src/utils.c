bd1082fa05424a99786d63e31140878a53198c1c
MicchelFAB <micchelfab@gmail.com>
2023-11-21 18:22:11 +0000

    day end

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,13 +6,13 @@
 /*   By: mamaral- <mamaral-@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:39:26 by mamaral-          #+#    #+#             */
-/*   Updated: 2023/11/20 15:45:14 by mamaral-         ###   ########.fr       */
+/*   Updated: 2023/11/21 18:21:58 by mamaral-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	ft_atoi_philo(char *str)
+int ft_atoi_philo(char *str)
 {
 	int r = 0;
 	int sg = 1;
@@ -30,6 +30,25 @@ int	ft_atoi_philo(char *str)
 		r = r * 10 + (*str - '0');
 		str++;
 	}
+	if (*str != '\0')
+		return (-1); 
 	return (sg * r);
 }
 
+int waiting(t_philo *philo, struct timeval start, long wait_time)
+{
+	struct timeval now;
+	long time;
+
+	while (1)
+	{
+		if (*(philo->dead) == 1 || philo->meal == 1)
+			return (-1);
+		gettimeofday(&now, NULL);
+		time = ((now.tv_sec - start.tv_sec) * 1000000 + now.tv_usec - start.tv_usec);
+		if(time >= wait_time)
+			break;
+		usleep(100);
+	}
+	return (0);
+}
